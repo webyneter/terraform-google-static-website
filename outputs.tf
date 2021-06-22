14b35f767f61e6688c49048313bee64db0dc07c7
Nikita Shupeyko <webyneter@gmail.com>
2021-06-22 11:47:32 +0300

    Rename the output value

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "bucket_name" {
   value = google_storage_bucket.website.name
 }
 
-output "service_account_private_key_filename" {
+output "bucket_service_account_private_key_filename" {
   value = local_file.website_service_account_private_key.filename
 }
