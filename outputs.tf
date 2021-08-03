60a42d8a5fada7527c19f5dc1af6d18d16b9b8d7
Nikita Shupeyko <webyneter@gmail.com>
2021-08-03 13:13:23 +0300

    Move all outputs to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,22 @@
+# =======
+# Storage
+# =======
+output "bucket_name" {
+  value = google_storage_bucket.website.name
+}
+
+output "bucket_service_account_id" {
+  value = google_service_account.website.account_id
+}
+
+output "bucket_service_account_private_key" {
+  value     = google_service_account_key.website.private_key
+  sensitive = true
+}
+
+# =======
+# Network
+# =======
 output "website_external_ip_name" {
   value = google_compute_global_address.website_external_ip.name
 }
